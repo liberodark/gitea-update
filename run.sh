8f5ab6bd924edf2d8fdaf3769d05f41167c63da3
liberodark <liberodark@gmail.com>
2020-03-26 11:15:22 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,17 +18,27 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 # RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
 #=================================================
 
-name=gitea
-version=1.11.3
+name="gitea"
+version="1.11.3"
 date=$(date +%Y.%m.%d_%H-%M-%S)
+old_version="3"
+destination="/opt/gitea/"
+lock="/tmp/gitea.lock"
+remove() { ls "$destination""$name"_old-* | head -n -"$old_version" | xargs rm -f; }
+
+exec 9>"${lock}"
+flock -n 9 || exit
 
 install(){
       echo "Downloading Gitea "$version""
       systemctl stop gitea
-      cd /opt/gitea || exit
-      mv gitea gitea_old-"date"
+      pushd "$destination" || exit
+      mv gitea gitea_old-"$date"
       wget -O "$name" "https://github.com/go-gitea/gitea/releases/download/v"$version"/gitea-"$version"-linux-amd64" &> /dev/null
       chmod +x gitea
+      echo "Old Gitea is Cleaned"
+      "remove"
+      popd || exit
       echo "Gitea "$version" is Installed"
       systemctl start gitea
       }
